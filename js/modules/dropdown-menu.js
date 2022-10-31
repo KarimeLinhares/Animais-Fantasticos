2f6f829f3e66d31bd4d047a5b614c2fa829bc4c7
KarimeLinhares <karime_muniz@hotmail.com>
2022-10-31 17:46:30 -0300

    dropdown Menu

---
+++ js/modules/dropdown-menu.js
@@ -0,0 +1,23 @@
+import outsideClick from "./outside-click.js";
+
+export default function initDropdownMenu() {
+  
+  //seleciona os submenus
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+  
+  //adiciona evento ao click de mouse, ou toque no mobile
+  dropdownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+      menu.addEventListener(userEvent, handleClick);
+    })
+  })
+
+  //função que adiciona a classe ativo ao menu, para mantê-lo ativo
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    })
+  }
+}
--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ import initAccordion from "./modules/accordion.js"
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
 
 initScroll();
 initAnimeScroll();
@@ -11,3 +13,5 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
+
