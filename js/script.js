cc9b42d68ffe19e45cdd24671f13989308f726a0
KarimeLinhares <karime_muniz@hotmail.com>
2022-10-31 10:44:11 -0300

    modal de login

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScroll from "./modules/scroll.js";
 import initAnimeScroll from "./modules/anime-scroll.js";
 import initAccordion from "./modules/accordion.js"
 import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
 
 initScroll();
 initAnimeScroll();
 initAccordion();
 initTabNav();
+initModal();
