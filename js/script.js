0f5d7557286c450fea96b2e00a2905d20911c7cf
KarimeLinhares <karime_muniz@hotmail.com>
2022-11-01 15:42:18 -0300

    menu mobile

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+
 
 
 initScroll();
@@ -14,4 +16,5 @@ initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
 
